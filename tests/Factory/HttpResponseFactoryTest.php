2f3f145c1de0dc0ae61622229438077ae12fb731
Ahmed Sallam <ahmed.sallam@aqarmap.com>
2020-01-03 13:31:46 +0200

    add HttpResponseFactory test

---
+++ tests/Factory/HttpResponseFactoryTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Tests\Repository;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use App\Factory\HttpResponseFactory;
+use Symfony\Component\HttpFoundation\Response;
+
+class HttpResponseFactoryTest extends KernelTestCase
+{
+    /**
+     * @var App\Factory\HttpResponseFactory
+     */
+    private $factory;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->factory = $kernel->getContainer()
+            ->get(HttpResponseFactory::class)
+        ;
+    }
+
+    /**
+     * @test
+     */
+    public function test_create()
+    {
+        $factory = $this->factory::create();
+
+        $this->assertInstanceOf(Response::class,  $factory);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+}
